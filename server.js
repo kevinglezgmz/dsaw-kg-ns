a0af4abcdfa9b21e73ab2084b8bdf7b7fcd54d51
Kevin Gonzalez <kevingonzalezgmz@gmail.com>
2020-11-08 15:53:04 -0600

    added index, login, signup and product views

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
