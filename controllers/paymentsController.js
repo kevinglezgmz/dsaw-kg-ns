e68eac2c2a768f890974e88c2279c48ad841cc71
natxsglez <44679189+natxsglez@users.noreply.github.com>
2020-11-22 17:34:50 -0600

    Files structure (#4)
    Back end file structure

---
+++ controllers/paymentsController.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const PATH = require('path');
+
+class PaymentsController{};
+
+module.exports = PaymentsController;
---
+++ controllers/usersController.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const PATH = require('path');
+
+class UsersController{};
+
+module.exports = UsersController;
