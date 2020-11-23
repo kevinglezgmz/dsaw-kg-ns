1e5c65803d49187ff58049f22b149beb6724bf01
Kevin Gonzalez <kevingonzalezgmz@gmail.com>
2020-11-22 22:11:52 -0600

    added the corresponding product routes and controller operations

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -24,6 +24,91 @@ class ProductsController {
     let addedProduct = await PRODUCTS_DB.insert(product);
     return addedProduct;
   }
+
+  async getAllProducts() {
+    let products = [];
+    let docs = await PRODUCTS_DB.list({ include_docs: true });
+    for (let entry of docs.rows) {
+      let product = {
+        productID: entry.doc.productID,
+        name: entry.doc.name,
+        price: entry.doc.price,
+        images: entry.doc.images,
+        category: entry.doc.category,
+      };
+      products.push(product);
+    }
+    return products;
+  }
+
+  async getProductsByCategory(category) {
+    const query = {
+      selector: {
+        category: { $eq: category },
+      },
+    };
+    let products = [];
+    let dbObject = await PRODUCTS_DB.find(query);
+    let docs = dbObject.docs;
+    console.log(docs);
+    for (let doc of docs) {
+      let product = {
+        productID: doc.productID,
+        name: doc.name,
+        price: doc.price,
+        images: doc.images,
+        category: doc.category,
+      };
+      products.push(product);
+    }
+    return products;
+  }
+
+  async getProductByID(id) {
+    const query = {
+      selector: {
+        productID: { $eq: id },
+      },
+    };
+    let dbObject = await PRODUCTS_DB.find(query);
+    let docs = dbObject.docs;
+    let product = docs[0];
+    return product;
+  }
+
+  async getProductsByNameOrDescription(stringToFind) {
+    console.log(stringToFind);
+    const query = {
+      selector: {
+        $or: [{ name: { $regex: `(?i)${stringToFind}` } }, { description: { $regex: `(?i)${stringToFind}` } }],
+      },
+    };
+    let products = [];
+    let dbObject = await PRODUCTS_DB.find(query);
+    let docs = dbObject.docs;
+    console.log(docs);
+    for (let doc of docs) {
+      let product = {
+        productID: doc.productID,
+        name: doc.name,
+        price: doc.price,
+        images: doc.images,
+        category: doc.category,
+      };
+      products.push(product);
+    }
+    return products;
+  }
+
+  async updateProduct(product) {
+    let updateStatus = await PRODUCTS_DB.insert(product);
+    return updateStatus;
+  }
+
+  async deleteProduct(product) {
+    let deleteStatus = await PRODUCTS_DB.destroy(product._id, product._rev);
+    return deleteStatus;
+  }
 }
 
 module.exports = ProductsController;
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -4,11 +4,70 @@ const productsController = require("../controllers/productsController");
 const productCtrlr = new productsController();
 const router = express();
 
+router.get("/", async (req, res) => {
+  if (req.query.name) {
+    let products = await productCtrlr.getProductsByNameOrDescription(req.query.name);
+    res.send(products);
+  } else {
+    let products = await productCtrlr.getAllProducts();
+    res.send(products);
+  }
+});
+
+router.get("/categories/:category", async (req, res) => {
+  let products = await productCtrlr.getProductsByCategory(req.params.category);
+  res.send(products);
+});
+
+router.get("/:id", async (req, res) => {
+  let product = await productCtrlr.getProductByID(req.params.id);
+  if (product) {
+    delete product["_id"];
+    delete product["_rev"];
+    res.send(product);
+  } else {
+    res.set("Content-Type", "application/json");
+    res.status(404).send({ message: "Product not found" });
+  }
+});
+
 router.post("/", async (req, res) => {
   console.log(req.body);
   //let product = productCtrlr.createProduct();
   let addedProduct = await productCtrlr.insertProduct(req.body);
   res.send(addedProduct);
+});
+
+router.put("/:id", async (req, res) => {
+  let productUpdatee = req.body;
+  let product = await productCtrlr.getProductByID(req.params.id);
+  res.set("Content-Type", "application/json");
+  if (product) {
+    Object.assign(product, productUpdatee);
+    let updateStatus = await productCtrlr.updateProduct(product);
+    if (updateStatus.ok) {
+      res.send({ message: "Product " + product.productID + " updated" });
+    } else {
+      res.status(400).send({ message: "Could not update the product" });
+    }
+  } else {
+    res.status(404).send({ message: "Product to update was not found" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  let product = await productCtrlr.getProductByID(req.params.id);
+  res.set("Content-Type", "application/json");
+  if (product) {
+    let deleteStatus = await productCtrlr.deleteProduct(product);
+    if (deleteStatus.ok) {
+      res.send({ message: "Product " + product.productID + " deleted" });
+    } else {
+      res.status(400).send({ message: "Could not delete the product" });
+    }
+  } else {
+    res.status(404).send({ message: "Product to delete was not found" });
+  }
 });
 
 module.exports = router;
