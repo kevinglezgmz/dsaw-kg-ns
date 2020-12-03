a83f9f0bf90d2a0488d7147074e856072406536c
kevinglezgmz <44679178+kevinglezgmz@users.noreply.github.com>
2020-12-02 19:37:02 -0600

    feature: user controller and user integration / index front-end and back-end integration (#7)
    * integrated user details and edit user
    * feature: integrated front-end and back-end for the index page

---
+++ public/js/index.js
@@ -0,0 +1,114 @@
+const APIURL = window.location.protocol + "//" + window.location.host + "/api";
+let products;
+
+function categoryProductToCardHTML(product) {
+  return `<div class="card category-card" onclick="goToProductsPageByCategory(event)" data-category="${product.category}">
+    <div class="image-box">
+      <img src="${product.images[0]}" class="card-img-top" alt="..." />
+    </div>
+    <div class="card-body">
+      <h5 style="text-align: center;"><a href="#">${product.category}</a></h5>
+    </div>
+  </div>`;
+}
+
+function productToHTML(product) {
+  return `<div class="card category-card" onclick="goToProductPage(event)" data-product-id="${product.productID}">
+    <div class="image-box">
+      <img src="${product.images[0]}" class="card-img-top" alt="..." />
+    </div>
+    <div class="card-body">
+      <h5 style="text-align: center;"><a href="#">${product.name}</a></h5>
+      <p>${product.description.length > 60 ? product.description.substr(0, 60) + "..." : product.description}</p>
+    </div>
+  </div>`;
+}
+
+function offerToHTML(product) {
+  return `<div class="card category-card" onclick="goToProductPage(event)" data-product-id="${product.productID}">
+    <div class="image-box">
+      <img src="${product.images[0]}" class="card-img-top" alt="..." />
+    </div>
+    <div class="card-body">
+      <h5 style="text-align: center;"><a href="#" style="color:red;">${product.name}</a></h5>
+      <p>${product.description.length > 60 ? product.description.substr(0, 60) + "..." : product.description}</p>
+    </div>
+  </div>`;
+}
+
+function goToProductsPageByCategory(event) {
+  event.preventDefault();
+  let category = event.path;
+  for (let ele of event.path) {
+    if (ele.getAttribute("data-category")) {
+      category = ele.getAttribute("data-category");
+      window.location.href = "/productSearch.html?category=" + category;
+    }
+  }
+}
+
+function goToProductPage(event) {
+  event.preventDefault();
+  let productID;
+  for (let ele of event.path) {
+    if (ele.getAttribute("data-product-id")) {
+      category = ele.getAttribute("data-product-id");
+      window.location.href = "/product.html?productID=" + category;
+    }
+  }
+}
+
+function categoryProductListToHTML(productList) {
+  let productsHTML = productList.reverse().map((product) => categoryProductToCardHTML(product));
+  return productsHTML.join("");
+}
+
+function productListToHTML(productList) {
+  let productsHTML = productList.map((product) => productToHTML(product));
+  return productsHTML.join("");
+}
+
+function offersListToHTML(offerList) {
+  let offersHTML = offerList.map((product) => offerToHTML(product));
+  return offersHTML.join("");
+}
+
+function setCategoryCards() {
+  let categories = {};
+  let categoryProducts = [];
+  for (let product of products.reverse()) {
+    if (categories[product.category]) continue;
+    categoryProducts.push(product);
+    categories[product.category] = true;
+  }
+  document.getElementById("categoryCards").innerHTML = categoryProductListToHTML(categoryProducts);
+}
+
+function setNewProducts() {
+  document.getElementById("newProductCards").innerHTML = productListToHTML(products.slice(0, 8));
+}
+
+function setOfferProducts() {
+  document.getElementById("offerCards").innerHTML = offersListToHTML(products.slice(8, 18));
+}
+
+function getAllProducts() {
+  sendHTTPRequest(
+    "/api/products/",
+    "",
+    HTTPMethods.get,
+    (data) => {
+      products = JSON.parse(data.data);
+      setCategoryCards();
+      setNewProducts();
+      setOfferProducts();
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  getAllProducts();
+});
--- public/js/user.js
+++ public/js/user.js
@@ -7,22 +7,43 @@ function userToHTML(user) {
         <p id="email">${user.email}</p>
         <p><b>Nombre:</b></p>
         <p id="name">${user.name + " " + user.lastName}</p>
-    </div>`
+    </div>`;
+}
+
+function getTokenValue(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
 
 function getUser() {
-  let urlParams = new URLSearchParams(window.location.search);
-  let email = urlParams.get('email');
-  let url = APIURL + '/users/' + email;
-  sendHTTPRequest(url, '', HTTTPMethods.get, (data) => {
-    let user = JSON.parse(data.data);
-    let userHTML = userToHTML(user);
-    document.getElementById("userData").innerHTML = userHTML;
-  }, (error) => {
-    document.getElementById("responseMSG").innerHTML = '<div class="alert alert-danger">' + error + '</div>';
-  })
+  let email = getTokenValue("user-email");
+  sendHTTPRequest(
+    "/api/users/" + email,
+    "",
+    HTTPMethods.get,
+    (response) => {
+      let user = JSON.parse(response.data);
+      let userHTML = userToHTML(user);
+      document.getElementById("userData").innerHTML = userHTML;
+    },
+    (error) => {
+      window.location.replace("/login.html");
+      document.getElementById("responseMSG").innerHTML = '<div class="alert alert-danger">' + error + "</div>";
+    }
+  );
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   getUser();
 });
